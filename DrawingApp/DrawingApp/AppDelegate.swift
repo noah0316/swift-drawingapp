06fe2eb2f11e4f0c210a31b3a13f7abe5ac85cd1
noah0316 <dev.noah0316@gmail.com>
2023-03-27 17:28:36 +0900

    DrawingApp 프로젝트 생성
    - Supported Destinations iPad로 제한
    - Device Orientation Landscape Left, Landscape Right로 제한

---
+++ DrawingApp/DrawingApp/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  DrawingApp
+//
+//  Created by Noah on 2023/03/27.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+}
+
