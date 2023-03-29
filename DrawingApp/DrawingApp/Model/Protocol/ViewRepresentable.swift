b0362f56047aeb63734d1f0339f4bf1348c5186d
noah0316 <dev.noah0316@gmail.com>
2023-03-29 17:46:16 +0900

    사용하지 않는 속성 제거
    - View를 표시하기 위해 id 속성은 필요하지 않다고 판단하여 제거

--- DrawingApp/DrawingApp/Model/Protocol/ViewRepresentable.swift
+++ DrawingApp/DrawingApp/Model/Protocol/ViewRepresentable.swift
@@ -8,15 +8,7 @@
 import Foundation
 
 protocol ViewRepresentable {
-    var id: Identifier { get }
     var frame: Frame { get set }
     var color: Color { get set }
     var alpha: Alpha { get set }
-    
-    init(
-        id: Identifier,
-        frame: Frame,
-        color: Color,
-        alpha: Alpha
-    )
 }
