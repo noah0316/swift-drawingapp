bf389bb8575273845be2dd24f9af52c1e9f4e9a4
noah0316 <dev.noah0316@gmail.com>
2023-03-30 11:47:00 +0900

    ViewController 추가
    - Rectangle 객체를 생성해 콘솔화면에 로그를 남길 뷰 컨트롤러 추가

---
+++ DrawingApp/DrawingApp/ViewController/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by Noah on 2023/03/29.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
