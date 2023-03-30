390326ab2faa02e98625950abe49d9f0f539d21f
noah0316 <dev.noah0316@gmail.com>
2023-03-30 11:54:37 +0900

    의존타입 추가
    - rectangle객체의 생성의 역할을 가지고 있는 rectangleFactory,
    - 로깅의 역할을 가지고 있는 Logger 속성 추가
    - 생성자로 의존 타입을 넘겨받도록 설정

--- DrawingApp/DrawingApp/ViewController/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController/ViewController.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    private let rectangleFactory: ViewRepresentableFactory?
+    private let logger: Loggable?
+    
+    init(rectangleFactory: ViewRepresentableFactory, logger: Loggable) {
+        self.rectangleFactory = rectangleFactory
+        self.logger = logger
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        self.rectangleFactory = nil
+        self.logger = nil
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
