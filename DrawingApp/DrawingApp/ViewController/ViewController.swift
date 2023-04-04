864da6741d11b550d06e94b7a4b813ace8c05815
noah0316 <dev.noah0316@gmail.com>
2023-04-04 11:38:13 +0900

    불필요한 guard문 제거

--- DrawingApp/DrawingApp/ViewController/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController/ViewController.swift
@@ -35,9 +35,6 @@ final class ViewController: UIViewController {
     }
     
     private func makeRectangle(numberOf times : Int) {
-        guard let rectangleFactory = self.rectangleFactory
-        else { return }
-        
         for _ in 0..<times {
             let rectangle = rectangleFactory.makeViewRepresentable()
             self.logger?.log(with: rectangle.description)
