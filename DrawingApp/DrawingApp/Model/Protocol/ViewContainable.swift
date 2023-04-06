d4382c3edd8074ee73c08dcbf06ef078d8fddfaf
noah0316 <dev.noah0316@gmail.com>
2023-04-06 11:53:44 +0900

    ViewContainable 프로토콜 추가
    - 뷰를 나타내는 모델을 포함할 수 있는 ViewContainable 프로토콜 추가

---
+++ DrawingApp/DrawingApp/Model/Protocol/ViewContainable.swift
@@ -0,0 +1,15 @@
+//
+//  ViewContainable.swift
+//  DrawingApp
+//
+//  Created by Noah on 2023/04/06.
+//
+
+import Foundation
+
+protocol ViewContainable {
+    /// reference: https://developer.apple.com/documentation/swift/array/append(_:)-1ytnt
+    func append(_ view: ViewRepresentable)
+    /// reference: https://developer.apple.com/documentation/swift/array/contains(_:)
+    func contains(_ view: ViewRepresentable) -> Bool
+}
