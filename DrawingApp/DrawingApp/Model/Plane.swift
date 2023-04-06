0835e0e7787d2e24dea42e0350080a50226b5e15
noah0316 <dev.noah0316@gmail.com>
2023-04-06 18:03:19 +0900

    ViewContainable 프로토콜 채택
    - ViewRepresentable을 포함할 수 있는 능력을 나타내는 ViewContainable 프로토콜 채택

--- DrawingApp/DrawingApp/Model/Plane.swift
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -14,3 +14,25 @@ final class Plane {
         self.views = [ViewRepresentable]()
     }
 }
+
+extension Plane: ViewContainable {
+    var count: Int {
+        return self.views.count
+    }
+    
+    subscript(index: Int) -> ViewRepresentable {
+        return self.views[index]
+    }
+    
+    func append(_ view: ViewRepresentable) {
+        self.views.append(view)
+    }
+    
+    func contains(_ view: ViewRepresentable) -> Bool {
+        return self.views.contains { $0.id == view.id }
+    }
+    
+    func contains(at position: Position) -> Bool {
+        return self.views.contains { $0.frame.position == position }
+    }
+}
