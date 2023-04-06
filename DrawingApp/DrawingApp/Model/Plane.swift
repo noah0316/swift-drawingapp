743295600b57d35173774a040074cb142c8597f9
noah0316 <dev.noah0316@gmail.com>
2023-04-06 18:01:42 +0900

    Plane 클래스 추가

---
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -0,0 +1,16 @@
+//
+//  Plane.swift
+//  DrawingApp
+//
+//  Created by Noah on 2023/04/04.
+//
+
+import Foundation
+
+final class Plane {
+    private var views: [ViewRepresentable]
+    
+    init() {
+        self.views = [ViewRepresentable]()
+    }
+}
