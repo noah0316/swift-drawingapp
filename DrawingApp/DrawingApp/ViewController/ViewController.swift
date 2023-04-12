f8eb7abe827d5ca9d42646652cb88d04d605d64b
noah0316 <dev.noah0316@gmail.com>
2023-04-12 16:25:17 +0900

    CanvasArea 설정
    - 뷰를 그릴 CanvasArea UI 관련 속성 설정
    - 뷰를 그릴 CanvasArea Layout 설정

--- DrawingApp/DrawingApp/ViewController/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController/ViewController.swift
@@ -11,6 +11,9 @@ final class ViewController: UIViewController {
     private let rectangleFactory: ViewRepresentableFactory?
     private let logger: Loggable?
     
+    // MARK: - UI properties
+    
+    @UseAutoLayout private var canvasArea: UIView = UIView()
     init(rectangleFactory: ViewRepresentableFactory, logger: Loggable) {
         self.rectangleFactory = rectangleFactory
         self.logger = logger
@@ -31,6 +34,37 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupUIAtrributes()
+        self.setupLayoutConstraints()
+    }
+    
+    private func setupUIAtrributes() {
+        self.setupCanvasAreaUIAttributes()
+    }
+    
+    private func setupLayoutConstraints() {
+        self.setupCanvasAreaLayoutConstraints()
+    }
+}
+
+extension ViewController {
+    private func setupCanvasAreaUIAttributes() {
+        self.canvasArea.backgroundColor = .white
+    }
+}
+
+extension ViewController {
+    private func setupCanvasAreaLayoutConstraints() {
+        self.view.addSubview(self.canvasArea)
+        
+        NSLayoutConstraint.activate(
+            [
+                self.canvasArea.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                self.canvasArea.topAnchor.constraint(equalTo: self.view.topAnchor),
+                self.canvasArea.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+                self.canvasArea.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8)
+            ]
+        )
     }
     
     }
