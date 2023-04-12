72bd41f6468785c2767e6b667b38a1a2caef63f2
noah0316 <dev.noah0316@gmail.com>
2023-04-12 16:19:48 +0900

    StringLiteral 추가
    - 문자열을 나타내는 StringLiteral 추가

---
+++ DrawingApp/DrawingApp/Global/Literal/StringLiteral.swift
@@ -0,0 +1,23 @@
+//
+//  StringLiteral.swift
+//  DrawingApp
+//
+//  Created by Noah on 2023/04/12.
+//
+
+import Foundation
+
+enum StringLiteral {
+    enum AddRectangleButton {
+        static let title = "사각형"
+    }
+    
+    enum BackgroundColorControl {
+        static let title = "배경색"
+        static let description = "#000000"
+    }
+    
+    enum AlphaControl {
+        static let title = "투명도"
+    }
+}
